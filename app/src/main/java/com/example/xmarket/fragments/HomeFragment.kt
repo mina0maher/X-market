33300662a762775e80243141133e41ce14f527f8
mina0maher <86135594+mina0maher@users.noreply.github.com>
2022-08-08 16:18:57 +0200

    making login&register UI

---
+++ app/src/main/java/com/example/xmarket/fragments/HomeFragment.kt
@@ -0,0 +1,9 @@
+package com.example.xmarket.fragments
+
+import androidx.fragment.app.Fragment
+
+
+class HomeFragment : Fragment() {
+
+
+}
---
+++ app/src/main/java/com/example/xmarket/fragments/ProductFragment.kt
@@ -0,0 +1,7 @@
+package com.example.xmarket.fragments
+
+import androidx.fragment.app.Fragment
+
+class ProductFragment : Fragment() {
+
+}
